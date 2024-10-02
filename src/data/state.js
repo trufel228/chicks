d878de59afa7598d07c45cd6753826662472acb3
trufel228 <kioji1@yandex.ru>
2024-10-02 17:28:46 +0800

    store

--- src/data/state.js
+++ src/data/state.js
@@ -1,71 +1,75 @@
-let state = {
-  profileData: {
-    postItems: [
-      { id: 1, mess: "доброго времени суток! сумерки гугугага", likes: 52 },
-      { id: 1, mess: "я пошел в школу, всем пака", likes: 42 },
-      { id: 1, mess: "я гуляю в лесу с собаками", likes: 1488 },
-      { id: 1, mess: "недавно прогал по деревьям", likes: 77 },
-      { id: 1, mess: "я проснулся, всем бобра", likes: 112 },
-      { id: 1, mess: "я пошел на улицу, чтобы выгуляться", likes: 325 },
-    ],
-    postUsers: [
-      {
-        id: 1,
-        name: "Xatab Minionov",
-        nickname: "@minionchik",
-        avatar: "ava.jpg",
-      },
-    ],
-    newPostText: "",
+let store = {
+  _state: {
+    profileData: {
+      postItems: [
+        { id: 1, mess: "доброго времени суток! сумерки гугугага", likes: 52 },
+        { id: 1, mess: "я пошел в школу, всем пака", likes: 42 },
+        { id: 1, mess: "я гуляю в лесу с собаками", likes: 1488 },
+        { id: 1, mess: "недавно прогал по деревьям", likes: 77 },
+        { id: 1, mess: "я проснулся, всем бобра", likes: 112 },
+        { id: 1, mess: "я пошел на улицу, чтобы выгуляться", likes: 325 },
+      ],
+      postUsers: [
+        {
+          id: 1,
+          name: "Xatab Minionov",
+          nickname: "@minionchik",
+          avatar: "ava.jpg",
+        },
+      ],
+      newPostText: "",
+    },
+
+    dialogData: {
+      messUsers: [
+        { id: 1, name: "Иван Иванов" },
+        { id: 2, name: "Дональд Трамп" },
+        { id: 3, name: "Билл Гейтс" },
+      ],
+      messItems: [
+        { id: 1, mess: "Привет, как дела?" },
+        { id: 2, mess: "Го завтра на Марс?" },
+        { id: 3, mess: "Жду мой новый чип" },
+      ],
+      newMessText: "",
+    },
+  },
+  reRender(state) {
+    return;
+  },
+  subscribe(observer) {
+    this.reRender = observer;
+  },
+  getState() {
+    return this._state
+  },
+  addPost() {
+    let newPost = {
+      id: 1488,
+      mess: this._state.profileData.newPostText,
+      likes: 0,
+    };
+    this._state.profileData.postItems.unshift(newPost);
+    this._state.profileData.newPostText = "";
+    this.reRender(this._state);
+  },
+  changePost(text) {
+    this._state.profileData.newPostText = text;
+    this.reRender(this._state);
   },
 
-  dialogData: {
-    messUsers: [
-      { id: 1, name: "Иван Иванов" },
-      { id: 2, name: "Дональд Трамп" },
-      { id: 3, name: "Билл Гейтс" },
-    ],
-    messItems: [
-      { id: 1, mess: "Привет, как дела?" },
-      { id: 2, mess: "Го завтра на Марс?" },
-      { id: 3, mess: "Жду мой новый чип" },
-    ],
-    newMessText: "",
+  addMess() {
+    let newMess = {
+      id: 1488,
+      mess: this._state.dialogData.newMessText,
+    };
+    this._state.dialogData.messItems.push(newMess);
+    this._state.dialogData.newMessText = "";
+    this.reRender(this._state);
+  },
+  changeMess(text) {
+    this._state.dialogData.newMessText = text;
+    this.reRender(this._state);
   },
 };
-export default state;
-let reRender = (state) => {
-  return
-};
-export let subscribe = (observer)=>{
-  reRender = observer
-} 
-
-export let addPost = () => {
-  let newPost = {
-    id: 1488,
-    mess: state.profileData.newPostText,
-    likes: 0,
-  };
-  state.profileData.postItems.unshift(newPost);
-  state.profileData.newPostText = "";
-  reRender(state);
-};
-export let changePost = (text) => {
-  state.profileData.newPostText = text;
-  reRender(state);
-};
-
-export let addMess = () => {
-  let newMess = {
-    id: 1488,
-    mess: state.dialogData.newMessText,
-  };
-  state.dialogData.messItems.push(newMess);
-  state.dialogData.newMessText = "";
-  reRender(state);
-};
-export let changeMess = (text) => {
-  state.dialogData.newMessText = text;
-  reRender(state);
-};
+export default store;
--- src/index.js
+++ src/index.js
@@ -1,22 +1,21 @@
-import state, { subscribe } from "./data/state";
+import store from "./data/state";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { addPost, addMess, changePost, changeMess } from "./data/state";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 export default function reRender(state) {
   root.render(
     <React.StrictMode>
-      <App state={state} addPost = {addPost} changePost = {changePost} addMess = {addMess} changeMess = {changeMess} />
+      <App state = {state} addPost = {store.addPost.bind(store)} changePost = {store.changePost.bind(store)} addMess = {store.addMess.bind(store)} changeMess = {store.changeMess.bind(store)} />
     </React.StrictMode>
   );
 }
 
-reRender(state)
-subscribe(reRender)
+reRender(store.getState())
+store.subscribe(reRender)
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
